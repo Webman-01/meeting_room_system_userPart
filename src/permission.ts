1fe6864c130d69a9779a18b71d11a9ed45764740
Webman-01 <yyui268@qq.com>
2024-02-04 00:55:22 +0800

    D26

--- src/permission.ts
+++ src/permission.ts
@@ -11,15 +11,19 @@ router.beforeEach(
   ) => {
     nprogress.start();
     const access_token = localStorage.getItem("access_token");
-    //1.如果用户未登录且不是要去登录或注册页面就跳到登录页面
-    if (!access_token && to.path !== "/login") {
-      next( { path: "/login" });
+    // 1.如果用户未登录且不是要去登录或注册页面就跳到登录页面
+    if (!access_token) {
+      if(to.path == '/login' || to.path == '/register'){
+        next()
+      }else{
+        next('/login')
+      }
     }
-    //2.如果登录成功就不再前往登录注册页面
-    if(access_token && (to.path=='login' || to.path=='/register')){
-        console.log('ok2');
-        
-        return false
+    else{
+        next()
     }
   }
 );
+router.afterEach(()=>{
+    nprogress.done()
+})
